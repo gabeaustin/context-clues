0f784deee9bf34b09ddb03fb44a3af944a85ca8d
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-09 16:33:00 -0600

    added friend, weapon, and location arrays

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let div = document.getElementById("mainDiv");
+    var div = document.getElementById("mainDiv");
+    var friendsArray = ["Ali",
+        "Fatima",
+        "Liam",
+        "Muhammad",
+        "Carter"];
+    var locationArray = ["Conservatory",
+        "Games Room",
+        "Wine Cellar",
+        "Attic",
+        "Pool Room",
+        "Psychomanteum",
+        "Sauna",
+        "Hearth Room",
+        "Parlor Room",
+        "Master Bedroom"
+    ];
+    var weaponArray = ["Cow Pies",
+        "Fart Gun",
+        "Mop Bucket",
+        "Roomba",
+        "Joystick",
+        "Fire Pocker",
+        "Hockey Stick",
+        "Morning Breath",
+        "Ancient Baby",
+        "Loud Monk",
+        "House Snake",
+        "Piranha Pond",
+        "Tricycle with Four Wheels",
+        "Backless Humpback Whale",
+        "Clown Shoes",
+        "Hot Grits",
+        "Christian Louboutin",
+        "Wine Bottle",
+        "Dumbbell",
+        "Cut Wire from the Doorbell"];
 
-    for (let i = 1; i <= 100; i++) {
-        let createH3Elements = document.createElement("h3");
-        let h3Text = document.createTextNode(`Accusation ${i}`);
+    for (var i = 1; i <= 100; i++) {
+        var createH3Elements = document.createElement("h3");
+        var h3Text = document.createTextNode(`Accusation ${i}`);
 
         createH3Elements.appendChild(h3Text);
         div.appendChild(createH3Elements);
+
+        div.addEventListener("click", function() {
+            console.log(`Accusation ${i}: I accuse FRIEND_NAME, with the WEAPON_NAME in the LOCATION_NAME!`)
+        })
     }
 })
 
